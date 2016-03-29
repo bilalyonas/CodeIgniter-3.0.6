74d361e45b5e63b2adb0ea774f87f34f26cd40d4
bilalyonas <u1258434@unimail.hud.ac.uk>
2016-03-30 00:18:10 +0100

    restriction page
    created a restriction page

---
+++ application/views/restricted.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to bilal</title>
+</head> 
+<body>
+
+<div id="container">
+	<h1>Your restricted to view this page</h1>
+	
+    <a href='<?php echo base_url()."index.php/main/login" ?>'>Back to login</a> 
+
+</div>
+
+</body>
+</html>
